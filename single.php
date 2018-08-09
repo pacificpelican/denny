97c58d1058838276123a5ddfe8fa7d7587502351
Daniel McKeown <2014@pacificpelican.us>
2018-08-09 15:21:30 -0700

    remove redundant (monolithic server-hydrated) post content on single posts, remove alert()

--- single.php
+++ single.php
@@ -20,7 +20,7 @@ window.onload = function () {
 			message: $postdata
 		},
 		mounted: function () {
-			alert();
+		//	alert();
 			let postID = "<?php echo $currentPostID ?>";
 			this.getWordPressPost(postID);
 		},
@@ -63,19 +63,19 @@ window.onload = function () {
 			</div>
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		// while ( have_posts() ) :
+		// 	the_post();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+		// 	get_template_part( 'template-parts/content', get_post_type() );
 
-			the_post_navigation();
+		// 	the_post_navigation();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		// 	// If comments are open or we have at least one comment, load up the comment template.
+		// 	if ( comments_open() || get_comments_number() ) :
+		// 		comments_template();
+		// 	endif;
 
-		endwhile; // End of the loop.
+		// endwhile; // End of the loop.
 		?>
 
 		</main><!-- #main -->
